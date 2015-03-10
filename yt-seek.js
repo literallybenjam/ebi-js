3f2c8a9c63759e4d1a06c970df37535461e5d0eb
literallybenjam <bjmns@me.com>
2015-03-09 23:11:21 -0700

    load, not play, and hopeful speed improvements

--- yt-seek.js
+++ yt-seek.js
@@ -10,11 +10,9 @@ var YT_Seek = {
 
 function onYouTubeIframeAPIReady() {
     var i;
-    YT_Seek.frames = document.querySelectorAll('iframe[src*="//youtube.com/embed/"], iframe[src*="//www.youtube.com/embed/"]');
     for (i = 0; i < YT_Seek.frames.length; i++) {
         YT_Seek.frames.item(i).yt_player = new YT.Player(YT_Seek.frames.item(i));
     }
-    YT_Seek.links = document.querySelectorAll('a[href^="#"][data-yt-seek-start], a[href^="#"][data-yt-seek-end]');
     for (i = 0; i < YT_Seek.links.length; i++) {
         YT_Seek.links.item(i).addEventListener("click", YT_Seek.cueVideo, false);
     }
@@ -28,11 +26,13 @@ YT_Seek.cueVideo = function() {
     if (!elt || !elt.yt_player) return;
     if (Number(this.dataset.ytSeekStart)) start = Number(this.dataset.ytSeekStart);
     if (Number(this.dataset.ytSeekEnd)) end = Number(this.dataset.ytSeekEnd);
-    if (start < end) elt.yt_player.playVideoById({videoId: elt.yt_player.getVideoData().video_id, startSeconds: start, endSeconds: end});
-    else elt.yt_player.playVideoById({videoId: elt.yt_player.getVideoData().video_id, startSeconds: start});
+    if (start < end) elt.yt_player.loadVideoById({videoId: elt.yt_player.getVideoData().video_id, startSeconds: start, endSeconds: end});
+    else elt.yt_player.loadVideoById({videoId: elt.yt_player.getVideoData().video_id, startSeconds: start});
 }
 
 YT_Seek.init = function() {
+    YT_Seek.frames = document.querySelectorAll('iframe[src*="//youtube.com/embed/"], iframe[src*="//www.youtube.com/embed/"]');
+    YT_Seek.links = document.querySelectorAll('a[href^="#"][data-yt-seek-start], a[href^="#"][data-yt-seek-end]');
     var tag = document.createElement('script');
     tag.type = "text/javascript";
     tag.src = "https://www.youtube.com/iframe_api";
