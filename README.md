476953817606c11e5db49024e785280c71f53bf3
literallybenjam <bjmns@me.com>
2015-03-20 13:38:48 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# RANDOMIZER.js
+
+A javascript script to randomly display one of several options.
+
+## About:
+
+RANDOMIZER.js finds every element with the class `randomizer` and sets the attribute `hidden` on every child but one, which is then displayed.
+
+## Usage:
+
+    Randomizer.init();
+
+Simply call `Randomizer.init` and the script will search the page for all elements with class `randomizer` and hide all but one of its children.
+
+## Endmatter:
+
+RANDOMIZER.js was coded by [@literallybenjam](https://twitter.com/literallybenjam). It is licensed under [the Unlicense](http://unlicense.org/UNLICENSE).
---
+++ randomizer.js
@@ -0,0 +1,19 @@
+/* jslint asi:true, browser:true */
+/* global CSSRule */
+
+var Randomizer = {
+    randomizers: undefined
+}
+
+Randomizer.init = function() {
+    Randomizer.randomizers = document.getElementsByClassName("randomizer");
+    for (var i = 0; i < Randomizer.randomizers.length; i++) {
+        var randomizer_items = Randomizer.randomizers.item(i).children;
+        if (randomizer_items) {
+            var random_index = Math.floor(Math.random() * randomizer_items.length);
+            for (var ii = 0; ii < randomizer_items.length; ii++) {
+                if (ii != random_index) randomizer_items.item(ii).setAttribute("hidden","");
+            }
+        }
+    }
+}
