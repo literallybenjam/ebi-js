606135752c698318e374c8d3cf2f78b69037fe7e
literallybenjam <bjmns@me.com>
2015-04-20 15:38:39 -0700

    First release; hopefully everything works ;)

--- README.md
+++ README.md
@@ -1,3 +1,66 @@
-#LOAD-JS
+# LOAD.js
 
 A async loader for JavaScript files!
+
+## Usage:
+
+Here's a sample HTML file using LOAD.js:
+
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Sample LOAD.js file</title>
+            <script type="text/javascript" src="/load.js"></script>
+            <script type="text/plain" id="load-data">
+                :: Here are some files ::
+                GET : /load/some_list_of_scripts.txt
+                    : /an_extra_script.js
+            </script>
+            <script type="text/javascript">
+                document.addEventListener("load-complete", function() {document.body.innerHTML = "All good!"}, false);
+            </script>
+        </head>
+        <body>
+            <p>Still loading…</p>
+        </body>
+    </html>
+
+The LOAD.js script may be included anywhere on the page. It will run automatically after all DOM content is loaded.
+
+LOAD.js requires an additional element, `#load-data`, an explanation for which follows. Although `#load-data` may technically be any element with an `id` of `load-data`, using a `<script type="text/plain">` element is recommended.
+
+### Loading JavaScript files individually:
+
+To load an individual JavaScript file, simply include the location of the file, as you would have it appear in a `src` attribute, on its own line inside the `#load-data` element. **NOTE: Right now, these must be absolute URLs, but support for relative ones is coming!** It may optionally be surrounded by whitespace and be preceded by a single colon. Here are some examples:
+
+    <script type="text/plain" id="load-data">
+        some_crazy_script.js
+        http://example.com/another_one.js
+        : colon_is_ignored.js
+    </script>
+
+### Loading JavaScript files from an external list:
+
+Alternatively, you can put the list of files (as they would appear in `#load-data`) inside a separate text file, and load it asynchronously. To do this, simply type `GET` or `POST` (depending on the method you wish to use), followed by a colon the URL of the file. This will send an XMLHttpRequest to access the file, whose contents will be read as plain text.
+
+You can place whitespace before the method, between the method and the colon, and after the colon.
+
+External text files can *also* contain `GET` and `POST` lines, in which case *those* files will also be read. LOAD.js doesn't currently check to make sure there aren't duplicates, or to prevent endless loops, so be careful when doing this!
+
+### Comments:
+
+You can include comments inside of the `#load-data` element by preceding a line with two colons. Whitespace can appear before, after, or even in-between these colons, and the entire line will be ignored.
+
+Comments *must* be on their own line, for now.
+
+### Running scripts after everything is loaded:
+
+Upon loading every script, LOAD.js dispatches the `load-complete` event to `document`. You can use `document.addEventListener("load-complete", someFunction, false)` to listen for this.
+
+### Other things to consider:
+
+LOAD.js creates a new global object, `Load`, in which it stores all of its data. Don't use `Load` for your own stuff to avoid breaking anything.
+
+## Endmatter:
+
+LOAD.js was coded by [@literallybenjam](https://twitter.com/literallybenjam). It is licensed under [the Unlicense](http://unlicense.org/UNLICENSE).
---
+++ load.js
@@ -0,0 +1,120 @@
+/* jslint asi:true, browser:true */
+
+var Load = {
+    event: undefined,
+    init: undefined,
+    getScripts: undefined,
+    processLines: undefined,
+    processRequest: undefined,
+    processScript: undefined,
+    requests: [],
+    requests_loaded: 0,
+    scripts: [],
+    scripts_loaded: 0
+}
+
+if (typeof(Event) === "function") Load.event = new Event("load-complete");
+else {
+    Load.event = document.createEvent("Event");
+    Load.event.initEvent("load-complete");
+}
+
+Load.getScripts = function() {
+    var i;
+    var tag;
+    if (!Load.scripts.length) {
+        document.dispatchEvent("load-complete");
+        return;
+    }
+    for (i = 0; i < Load.scripts.length; i++) {
+        tag = document.createElement('script');
+        tag.addEventListener("load", Load.processScript, false);
+        tag.type = "text/javascript";
+        tag.src = Load.scripts[i];
+        tag.dataset.loadIndex = i;
+        document.scripts.item(0).parentNode.insertBefore(tag, document.scripts.item(0));
+    }
+}
+
+Load.processLines = function(text) {
+
+    var lines;
+    var line_type;
+    var i;
+    var j;
+    var k;
+    var request;
+
+    lines = text.split("\n");
+    for (i = 0; i < lines.length; i++) {
+
+        j = 0;
+        if (lines[i].trim() === "" || /^\s*:\s*:/.test(lines[i])) continue;
+        else if (/^\s*GET\s*:/.test(lines[i].toUpperCase())) line_type = "GET";
+        else if (/^\s*POST\s*:/.test(lines[i].toUpperCase())) line_type = "POST";
+        else line_type = "";
+
+        switch (line_type) {
+
+            case "GET":
+                while (/\s/.test(lines[i].charAt(j))) j++;
+                j += 3;  //  GET
+                while (/\s/.test(lines[i].charAt(j))) j++;
+                j++;  //  colon
+                k = Load.requests.length;
+                Load.requests[k] = new XMLHttpRequest();
+                Load.requests[k].open("GET", lines[i].substr(j).trim(), true);
+                Load.requests[k].responseType = "text";
+                Load.requests[k].overrideMimeType("text/plain");
+                Load.requests[k].loadIndex = k;
+                Load.requests[k].addEventListener("load", Load.processRequest, false);
+                break;
+
+            case "POST":
+                while (/\s/.test(lines[i].charAt(j))) j++;
+                j += 4;  //  POST
+                while (/\s/.test(lines[i].charAt(j))) j++;
+                j++;  //  colon
+                k = Load.requests.length;
+                Load.requests[k] = new XMLHttpRequest();
+                Load.requests[k].open("POST", lines[i].substr(j).trim(), true);
+                Load.requests[k].responseType = "text";
+                Load.requests[k].overrideMimeType("text/plain");
+                Load.requests[k].loadIndex = k;
+                Load.requests[k].addEventListener("load", Load.processRequest, false);
+                break;
+
+            default:
+                while (/\s/.test(lines[i].charAt(j))) j++;
+                if (lines[i].charAt(j) == ":") j++;
+                Load.scripts[Load.scripts.length] = lines[i].substr(j).trim();
+                break;
+
+        }
+
+    }
+
+    for (i = 0; i < Load.requests.length; i++) {
+        if (Load.requests.readyState == 1) Load.requests.send();
+    }
+
+}
+
+Load.processRequest = function() {
+    Load.processLines(this.responseText);
+    Load.requests_loaded |= (1 << this.loadIndex);
+    if (Load.requests_loaded === ~(~0 << Load.requests.length)) Load.getScripts();
+}
+
+Load.processScript = function() {
+    Load.scripts_loaded |= (1 << this.dataset.loadIndex);
+    if (Load.scripts_loaded === ~(~0 << Load.scripts.length)) document.dispatchEvent("load-complete");
+}
+
+Load.init = function() {
+    if (!document.getElementById("load-data")) document.dispatchEvent("load-complete");
+    else if (document.getElementById("load-data").tagName.toUpperCase() === "SCRIPT") Load.processLines(document.getElementById("load-data").text);
+    else Load.processLines(document.getElementById("load-data").textContent);
+}
+
+document.addEventListener("DOMContentLoaded", Load.init, false);
