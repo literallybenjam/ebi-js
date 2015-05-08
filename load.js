d446f7ea674f3216e83b491312fb2876fe13f9b1
literallybenjam <bjmns@me.com>
2015-05-08 03:27:30 -0700

    nother typofix

--- load.js
+++ load.js
@@ -23,7 +23,7 @@ else {
 
 Load.expandURL = function(url, base) {
     if (url.substr(0,2) === "/\/" || /^[a-zA-Z0-9\-+]*:/.test(url)) return url;
-    else if (url[0] === "/" && base.lastIndexOf("/") !== -1) return base.lastIndexOf("/") + 1 + url;
+    else if (url[0] === "/" && base.lastIndexOf("/") !== -1) return base.substring(0, base.lastIndexOf("/") + 1) + url;
     else if (url[0] === "/") return base + url;
     else return base + "/" + url;
 }
