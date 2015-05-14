280b299b1b4f0a694d2e9fe61559a7ad7b5dd502
literallybenjam <bjmns@me.com>
2015-05-14 04:11:30 -0700

    Initial commit, yo

---
+++ README.md
@@ -0,0 +1,11 @@
+# SCROLL.js
+
+JavaScript to do some real smooth scrolling, yo.
+
+## Usage:
+
+Just include the script in your document somewhere. It'll handle the rest.
+
+## Endmatter:
+
+SCROLL.js was coded by [@literallybenjam](https://twitter.com/literallybenjam). It is licensed under [the Unlicense](http://unlicense.org/UNLICENSE).
---
+++ scroll.js
@@ -0,0 +1,68 @@
+/* jslint asi:true, browser:true */
+
+var Scroll = {
+    hash: "#",
+    element: null,
+    handleEvent: undefined,
+    source: null,
+    push_state: false,
+    advance: undefined,
+    target: 0,
+    velocity: 0,
+};
+
+Scroll.handleEvent = function(e) {
+
+    switch (e.type) {
+
+        case "click":
+            var a;
+            var n = e.target;
+            do if (n.nodeName.toUpperCase() === "A") a = n;
+            while (!a && n == n.parentNode);
+            if (!a || !document.getElementById(a.hash.substr(1))) return;
+            Scroll.source = a;
+            Scroll.element = document.getElementById(a.hash.substr(1));
+            Scroll.target = Scroll.element.getBoundingClientRect().top + window.scrollY;
+            Scroll.velocity = 0;
+            Scroll.hash = a.hash;
+            Scroll.push_state = true;
+            window.requestAnimationFrame(Scroll.advance);
+            e.preventDefault();
+            break;
+
+        case "popstate":
+            if (!window.location.hash || !document.getElementById(window.location.hash.substr(1))) return;
+            Scroll.source = null;
+            Scroll.element = document.getElementById(window.location.hash.substr(1));
+            Scroll.target = Scroll.element.getBoundingClientRect().top + window.scrollY;
+            Scroll.velocity = 0;
+            Scroll.hash = window.location.hash;
+            Scroll.push_state = false;
+            window.requestAnimationFrame(Scroll.advance);
+            break;
+
+    }
+
+}
+
+Scroll.advance = function() {
+    var max_scroll = window.scrollMaxY;
+    if (max_scroll === undefined) max_scroll = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var current_location = window.scrollY + window.innerHeight / 3;
+    if (Math.abs(current_location - Scroll.target) > 1 && max_scroll - window.scrollY >= Scroll.velocity && -1 * window.scrollY <= Scroll.velocity) {
+        Scroll.velocity = (Scroll.velocity + (Scroll.target - current_location) * 1023 / document.body.scrollHeight) / 2;
+        if (Scroll.velocity > 0) window.scrollBy(0, Math.ceil(Scroll.velocity));
+        else window.scrollBy(0, Math.floor(Scroll.velocity));
+        window.requestAnimationFrame(Scroll.advance);
+    }
+    else {
+        window.scrollBy(0, Scroll.target - Scroll.location);
+        Scroll.velocity = 0;
+        if (Scroll.push_state) window.history.pushState(null, "", Scroll.hash);
+    }
+}
+
+
+document.addEventListener("click", Scroll, true);
+window.addEventListener("popstate", Scroll, true);
